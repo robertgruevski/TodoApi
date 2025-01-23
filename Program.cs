7f0682e107a0f35c7d02f88ff4907875d5345552
robertgruevski <robertgruevski@outlook.com>
2025-01-23 13:14:58 -0500

    Get completed todos, get todo by Id

--- Program.cs
+++ Program.cs
@@ -12,6 +12,14 @@ app.MapGet("/", () => "TodoApi");
 
 app.MapGet("/todoitems", async (TodoDb db) => await db.Todos.ToListAsync());
 
+app.MapGet("/todoitems/complete", async (TodoDb db) => await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+	await db.Todos.FindAsync(id)
+		is Todo todo
+			? Results.Ok(todo)
+			: Results.NotFound());
+
 app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
 {
 	db.Todos.Add(todo);
