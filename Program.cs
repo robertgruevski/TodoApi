4169c9a35c7acbe722ba6f8ff966ac8907ea785d
robertgruevski <robertgruevski@outlook.com>
2025-01-23 14:06:33 -0500

    Adjustments to the method body for GetAllTodos, GetCompleteTodos, and GetTodo.

--- Program.cs
+++ Program.cs
@@ -18,23 +18,17 @@ todoItems.MapPost("/", CreateTodo);
 todoItems.MapPut("/{id}", UpdateTodo);
 todoItems.MapDelete("/{id}", DeleteTodo);
 
-static async Task<IResult> GetAllTodos(TodoDb db)
-{
-	return TypedResults.Ok(await db.Todos.ToArrayAsync());
-}
+static async Task<IResult> GetAllTodos(TodoDb db) => 
+	TypedResults.Ok(await db.Todos.ToArrayAsync());
 
-static async Task<IResult> GetCompleteTodos(TodoDb db)
-{
-	return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).ToListAsync());
-}
+static async Task<IResult> GetCompleteTodos(TodoDb db) => 
+	TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
-static async Task<IResult> GetTodo(int id, TodoDb db)
-{
-	return await db.Todos.FindAsync(id)
+static async Task<IResult> GetTodo(int id, TodoDb db) => 
+	await db.Todos.FindAsync(id)
 		is Todo todo
 			? TypedResults.Ok(todo)
 			: TypedResults.NotFound();
-}
 
 static async Task<IResult> CreateTodo(Todo todo, TodoDb db)
 {
