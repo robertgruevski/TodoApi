39202445eb2b73ac6a5a9a29c687974b99f314a8
robertgruevski <robertgruevski@outlook.com>
2025-01-23 13:14:22 -0500

    Get todos and post todo

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+// builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "TodoApi");
+
+app.MapGet("/todoitems", async (TodoDb db) => await db.Todos.ToListAsync());
+
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+	db.Todos.Add(todo);
+	await db.SaveChangesAsync();
+
+	return Results.Created($"/todoitems/{todo.Id}", todo);
+});
 
 app.Run();
--- Todo.cs
+++ Todo.cs
@@ -4,6 +4,6 @@
 	{
 		public int Id { get; set; }
 		public string? Name { get; set; }
-		public string IsComplete { get; set; }
+		public bool IsComplete { get; set; }
 	}
 }
