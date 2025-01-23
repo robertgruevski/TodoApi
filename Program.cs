78e3500f35af5f7c1a8997f32ac0b37bacd457c6
robertgruevski <robertgruevski@outlook.com>
2025-01-23 14:16:32 -0500

    Introducing TodoDTO

--- Program.cs
+++ Program.cs
@@ -18,34 +18,48 @@ todoItems.MapPost("/", CreateTodo);
 todoItems.MapPut("/{id}", UpdateTodo);
 todoItems.MapDelete("/{id}", DeleteTodo);
 
-static async Task<IResult> GetAllTodos(TodoDb db) => 
-	TypedResults.Ok(await db.Todos.ToArrayAsync());
-
-static async Task<IResult> GetCompleteTodos(TodoDb db) => 
-	TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).ToListAsync());
-
-static async Task<IResult> GetTodo(int id, TodoDb db) => 
-	await db.Todos.FindAsync(id)
-		is Todo todo
-			? TypedResults.Ok(todo)
-			: TypedResults.NotFound();
-
-static async Task<IResult> CreateTodo(Todo todo, TodoDb db)
+static async Task<IResult> GetAllTodos(TodoDb db)
 {
-	db.Todos.Add(todo);
-	await db.SaveChangesAsync();
-
-	return TypedResults.Created($"/todoitems/{todo.Id}", todo);
+	return TypedResults.Ok(await db.Todos.Select(x => new TodoItemDTO(x)).ToArrayAsync());
 }
 
-static async Task<IResult> UpdateTodo(int id, Todo inputTodo, TodoDb db)
+static async Task<IResult> GetCompleteTodos(TodoDb db)
+{
+	return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).Select(x => new TodoItemDTO(x)).ToListAsync());
+}
+
+static async Task<IResult> GetTodo(int id, TodoDb db)
+{
+	return await db.Todos.FindAsync(id)
+		is Todo todo
+			? TypedResults.Ok(new TodoItemDTO(todo))
+			: TypedResults.NotFound();
+}
+
+static async Task<IResult> CreateTodo(TodoItemDTO todoItemDTO, TodoDb db)
+{
+	var todoItem = new Todo
+	{
+		IsComplete = todoItemDTO.IsComplete,
+		Name = todoItemDTO.Name
+	};
+
+	db.Todos.Add(todoItem);
+	await db.SaveChangesAsync();
+
+	todoItemDTO = new TodoItemDTO(todoItem);
+
+	return TypedResults.Created($"/todoitems/{todoItem.Id}", todoItemDTO);
+}
+
+static async Task<IResult> UpdateTodo(int id, TodoItemDTO todoItemDTO, TodoDb db)
 {
 	var todo = await db.Todos.FindAsync(id);
 
 	if (todo is null) return TypedResults.NotFound();
 
-	todo.Name = inputTodo.Name;
-	todo.IsComplete = inputTodo.IsComplete;
+	todo.Name = todoItemDTO.Name;
+	todo.IsComplete = todoItemDTO.IsComplete;
 
 	await db.SaveChangesAsync();
 
